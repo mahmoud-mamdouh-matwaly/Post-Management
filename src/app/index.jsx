3c457a4ffddd65f7541f51e2386b3ad7af035339
mahmoud mamdouh metwally <mahmoud.m.alawady@gmail.com>
2023-05-31 23:43:48 +0300

    fix: to add edit route and change main route

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,23 +1,41 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useMemo } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import BaseLayOut from 'layout';
 import { Spin } from 'antd';
+import getStore from 'store';
+import { Provider as StoreProvider } from 'react-redux';
+
 const PostsPage = lazy(() => import('pages/posts'));
+const EditPostPage = lazy(() => import('pages/posts/edit'));
 
 function App() {
+  const store = useMemo(() => {
+    return getStore();
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<BaseLayOut />}>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Spin />}>
-              <PostsPage />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
+    <StoreProvider store={store}>
+      <Routes>
+        <Route path="/posts-management" element={<BaseLayOut />}>
+          <Route
+            path="/posts-management"
+            element={
+              <Suspense fallback={<Spin />}>
+                <PostsPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/posts-management/edit-post/:id"
+            element={
+              <Suspense fallback={<Spin />}>
+                <EditPostPage />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </StoreProvider>
   );
 }
 
