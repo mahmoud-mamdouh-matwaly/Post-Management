cc5ff80cef45d2a3f61048328211dc239d3096c3
mahmoud mamdouh metwally <mahmoud.m.alawady@gmail.com>
2023-06-11 07:57:36 +0300

    test(posts): add e2e to view modal

--- cypress/e2e/posts/posts-page-spec.cy.js
+++ cypress/e2e/posts/posts-page-spec.cy.js
@@ -1,7 +1,12 @@
 import postsPage from '../../fixtures/selector/posts';
-import { POSTS_RESPONSE, HEAD_POSTS_COLUMNS_LIST, ROW_VALUES_LIST } from '../../fixtures/selector/samples';
+import {
+  POSTS_RESPONSE,
+  HEAD_POSTS_COLUMNS_LIST,
+  ROW_VALUES_LIST,
+  LIST_OF_LABEL_NAME,
+} from '../../fixtures/selector/samples';
 
-const { assertions, selectors } = postsPage;
+const { assertions, selectors, actions } = postsPage;
 
 describe('Posts Page', () => {
   beforeEach(() => {
@@ -10,6 +15,7 @@ describe('Posts Page', () => {
   describe('Check posts page is render', () => {
     it('check page heading is render with action buttons', () => {
       assertions.assertElementIsVisible(selectors?.pageTitle);
+
       assertions.assertElementTextContains(selectors?.pageTitle, 'Posts');
     });
 
@@ -46,15 +52,52 @@ describe('Posts Page', () => {
         cy.wrap(textColumnsHead).should('deep.equal', HEAD_POSTS_COLUMNS_LIST);
       });
 
-      it('check posts table is render with values at row', () => {
+      it('check posts table is render with values at row and actions', () => {
         cy.wait('@requestPosts');
         const textRowValue = [];
         assertions.assertElementsChildrenCountEquals('.ant-table-tbody > .ant-table-row', 2);
-
+        assertions.assertElementIsVisible(selectors?.deleteBtn);
+        assertions.assertElementIsVisible(selectors?.editBtn);
+        assertions.assertElementIsVisible(selectors?.viewBtn);
         assertions
           .assertsElementsTableItems('table', '.ant-table-tbody tr:nth-child(2) > .ant-table-cell', 3)
           .each($li => textRowValue.push($li.text()));
         cy.wrap(textRowValue).should('deep.equal', ROW_VALUES_LIST);
+      });
+      describe('Check view modal is visible when click view button', () => {
+        beforeEach(() => {
+          actions.clickSpecificElement(selectors.viewBtn, 0);
+          assertions.assertElementIsVisible(selectors?.viewModal);
+        });
+ 
+        describe('Check view modal is have data and close it', () => {
+          it('check all label is render with correct label name', () => {
+            const labelNames = [];
+
+            cy.get(selectors.postForm)
+              .find('label')
+              .each($input => {
+                labelNames.push($input.attr('title'));
+              });
+            cy.wrap(labelNames).should('deep.equal', LIST_OF_LABEL_NAME);
+          });
+
+          it('check all input is render with correct name attribute', () => {
+            assertions.assertElementIsVisible(selectors.titleInput);
+            assertions.assertElementIsVisible(selectors.descriptionTextarea);
+          });
+
+          it('view modal should have values', () => {
+            assertions.assertInputHaveValue(selectors.titleInput, 'new post');
+            assertions.assertInputHaveValue(selectors.descriptionTextarea, 'new Description');
+          });
+
+          it('click close icon should modal not visible', () => {
+            assertions.assertElementIsExists(selectors?.closeIconModal);
+            actions.clickAction(selectors?.closeIconModal);
+            assertions.assertNotVisible(selectors?.viewModal);
+          });
+        });
       });
     });
   });
--- cypress/utils/cypress-helpers.js
+++ cypress/utils/cypress-helpers.js
@@ -2,11 +2,20 @@
 
 export default {
   testId: id => `[data-testid=${id}]`,
+  className: name => `[class=${name}]`,
   assertElementExistsAttr: selector => cy.get(selector).should('have.attr', 'data-testid'),
+  assertElementExists: selector => cy.get(selector).should('exist'),
   assertElementNotExists: selector => cy.get(selector).should('not.exist'),
   click: selector => cy.get(selector).click({ force: true, multiple: true }),
   assertVisibleText: text => cy.contains(text).should('be.visible'),
+  assertNotVisible: selector => cy.get(selector).should('not.be.visible'),
   assertElementTextContains: (selector, text) => cy.get(selector).should('contain.text', text),
   assertsElementsTableItems: (selector, child, count) => cy.get(selector).get(child).should('have.length', count),
   assertElementsChildrenCountEquals: (selector, count) => cy.get(selector).should('have.length', count),
+  clickElementByIndex: (selector, index) =>
+    cy.get(selector).each((element, elementIndex) => {
+      if (elementIndex === index) cy.wrap(element).click();
+    }),
+  assertShouldHaveValue: (selector, value) => cy.get(selector).should('have.value', value),
+
 };
